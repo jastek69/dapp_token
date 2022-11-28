367b2da6830f87c84bd1bf6eadda0e2a57f9453b
John <jastek@SEBEK.localdomain>
2022-11-27 23:38:19 -0800

    5. Token Transfers

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -11,12 +11,42 @@ contract Token {
 
     // Track balances
     mapping(address => uint256) public balanceOf;
+    
     // Send tokens
+    event Transfer(     // Transfer Event ERC-20 requirement: https://ethereum.org/en/developers/docs/standards/tokens/erc-20/#events
+        address indexed from,
+        address indexed to,
+        uint256 value
+    ); 
 
-    constructor(string memory _name, string memory _symbol, uint256 _totalSupply) {
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint256 _totalSupply
+    ) {
         name = _name;
         symbol = _symbol;
         totalSupply = _totalSupply* (10**decimals); // 1,000,000 x 10^18;
         balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address _to, uint256 _value)
+        public 
+        returns (bool success) 
+    {
+        // Require that sender has enough tokens to spend - ERC20 requirement
+        require(balanceOf[msg.sender] >= _value);
+        require(_to != address(0));
+
+        // Deduct tokens from spender
+        balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
+
+        // Credit tokens to receiver
+        balanceOf[_to] = balanceOf[_to] + _value;
+        
+        //Emit transfer event
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
     }
 }
--- test/Token.js
+++ test/Token.js
@@ -6,7 +6,7 @@ const tokens = (n) => {
 }
 
 describe('Token', ()=> {        // tests go inside here ... see https://hardhat.org/tutorial/testing-contracts
-    let token, accounts, deployer
+    let token, accounts, deployer, receiver
     
     beforeEach(async() => {
         // Fetch Token from Blockchain
@@ -15,7 +15,8 @@ describe('Token', ()=> {        // tests go inside here ... see https://hardhat.
         
         // Get accounts in the test suite
         accounts = await ethers.getSigners()
-        deployer = accounts[0]        
+        deployer = accounts[0]
+        receiver = accounts[1]        
     })
 
     describe('Deployment', () => {
@@ -24,26 +25,71 @@ describe('Token', ()=> {        // tests go inside here ... see https://hardhat.
         const decimals = '18'
         const totalSupply = tokens('1000000')
 
-        it('has correct name', async () => {    // Read Token name. Check that name is correct ... see Chai matchers: https://ethereum-waffle.readthedocs.io/en/latest/matchers.html 
+        it('Has correct name', async () => {    // Read Token name. Check that name is correct ... see Chai matchers: https://ethereum-waffle.readthedocs.io/en/latest/matchers.html 
             expect(await token.name()).to.equal(name)
         })
     
-        it('has correct symbol', async () => {  // Read Token symbol. Check that symbol is correct 
+        it('Has correct symbol', async () => {  // Read Token symbol. Check that symbol is correct 
             expect(await token.symbol()).to.equal(symbol)
         })
     
-        it('has correct decimals', async () => {  // Read Token decimals 
+        it('Has correct decimals', async () => {  // Read Token decimals 
             expect(await token.decimals()).to.equal(decimals)
         })
     
-        it('has correct total supply', async () => {  // Read Token total supply - converts 1000000 to Ether = 1000000000000000000000000
+        it('Has correct total supply', async () => {  // Read Token total supply - converts 1000000 to Ether = 1000000000000000000000000
             expect(await token.totalSupply()).to.equal(totalSupply)
         })
 
-        it('assgns total supply to deployer', async () => {
+        it('Assigns total supply to deployer', async () => {
             expect(await token.balanceOf(deployer.address)).to.equal(totalSupply)    
         })
 
-    })
 
+    describe('Sending Tokens', () => {
+        let amount, transaction, result
+
+        describe ('Success', () => {     // Success
+
+            beforeEach(async () => {        // Transfer tokens
+                amount = tokens(100)  
+                transaction = await token.connect(deployer).transfer(receiver.address, amount) // need to connect wallet in order to pay a fee as doing an action - sending tokens
+                result = await transaction.wait()
+            })    
+            
+            it('Transfers token balances', async () => {
+                // Ensure that tokens were transferred (balance changes)
+                expect(await token.balanceOf(deployer.address)).to.equal(tokens(999900))
+                expect(await token.balanceOf(receiver.address)).to.equal(amount)
+            })
+    
+            it('Emits a Transfer event', async () => {
+                //Checking Events
+                const event = result.events[0]
+                console.log(event)
+                expect(event.event).to.equal('Transfer')
+                
+                //Checking Arguments 
+                const args = event.args
+                expect(args.from).to.equal(deployer.address)
+                expect(args.to).to.equal(receiver.address)
+                expect(args.value).to.equal(amount)
+            })
+
+            describe('Failure', () => {
+                it('Rejects insufficient balances', async () => {
+                    // Transfer more tokens than deployer has - 10M
+                    const invalidAmount = tokens(10000000)
+                    await expect(token.connect(deployer).transfer(receiver.address, invalidAmount)).to.be.reverted
+                })
+
+                it('Rejects invalid recipient', async () => {
+                    const amount = tokens(100)
+                    await expect(token.connect(deployer).transfer('0x0000000000000000000000000000000000000000', amount)).to.be.reverted
+                })
+
+            })
+        })       
+    })
+    })
 })
